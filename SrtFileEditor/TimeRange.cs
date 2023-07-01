32d423d22b88ac9e9f06f336035b66089f013b4c
Jakub Becker <jakub.becker@banqsoft.pl>
2023-07-01 14:25:14 +0200

    Add shift method

--- SrtFileEditor/TimeRange.cs
+++ SrtFileEditor/TimeRange.cs
@@ -6,12 +6,20 @@ internal class TimeRange
     public const int MilisecondsInMinute = MilisecondsInSecond * 60;
     public const int MilisecondsInHour = MilisecondsInMinute * 60;
     private const string TimeSeparator = "-->";
+
     public long StartTime { get; set; }
     public long EndTime { get; set; }
     public TimeRange(long startTime, long endTime)
     {
         StartTime = startTime;
         EndTime = endTime;
+    }
+
+    public void Shift(long amountMiliseconds)
+    {
+        StartTime += amountMiliseconds;
+        EndTime += amountMiliseconds;
+    }
     }
     public static TimeRange Convert(string stimeRange)
     {
