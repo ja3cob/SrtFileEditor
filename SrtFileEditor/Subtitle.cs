9b1eedf0884ee3f7335f20885e478db28b8cf296
Jakub Becker <jakub.becker@banqsoft.pl>
2023-07-01 14:26:06 +0200

    Change var name

--- SrtFileEditor/Subtitle.cs
+++ SrtFileEditor/Subtitle.cs
@@ -3,13 +3,13 @@
     internal class Subtitle
     {
         public int Id { get; set; }
-        public TimeRange Range { get; set; }
+    public TimeRange TimeRange { get; set; }
         public string Text { get; set; }
 
         public Subtitle(int id, TimeRange range, string text)
         {
             Id = id;
-            Range = range;
+        TimeRange = range;
             Text = text;
         }
     }
